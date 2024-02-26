88278dcc52b050735983164aaab338f56dd618af
moeebii <ebiim621@newschool.edu>
2024-02-25 20:57:45 -0500

    japanese

--- script.js
+++ script.js
@@ -2,23 +2,22 @@
 
 // Function to check if the screen width is greater than 768 pixels (tablet or desktop)
 function isDesktopOrTablet() {
-    return window.innerWidth > 768;
+  return window.innerWidth > 768;
+}
+
+// Function to redirect to the specified page
+function redirectIfDesktopOrTablet() {
+  if (isDesktopOrTablet()) {
+    window.location.href = "https://moeebii.github.io/moeebiiworks/";
   }
-  
-  // Function to redirect to the specified page
-  function redirectIfDesktopOrTablet() {
-    if (isDesktopOrTablet()) {
-      window.location.href = "https://moeebii.github.io/moeebiiworks/";
-    }
-  }
-  
-  // Call the redirection function when the window is loaded
-  window.onload = function () {
-    redirectIfDesktopOrTablet();
-  };
-  
-  // Call the redirection function when the window is resized
-  window.onresize = function () {
-    redirectIfDesktopOrTablet();
-  };
-  
+}
+
+// Call the redirection function when the window is loaded
+window.onload = function () {
+  redirectIfDesktopOrTablet();
+};
+
+// Call the redirection function when the window is resized
+window.onresize = function () {
+  redirectIfDesktopOrTablet();
+};
